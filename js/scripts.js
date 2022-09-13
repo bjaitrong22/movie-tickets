b2def499c9492ff0dedcc1be5d51c4db43d7405b
Bai Jaitrong <bjaitrong@comcast.net>
2022-09-12 22:22:20 -0600

    Debug function by adding this. to name of variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-function Tickets(time, age)  {
+function Tickets(time, age, release)  {
   this.time = time;
   this.release = release;
   this.age = age;
   this.price = 10;
-}
+} 
 
 Tickets.prototype.getPrice = function() {
-  if( this.age >= 65 || this.time === "matinee" || release === "nonFirstRelease") {
+  if( this.age >= 65 || this.time === "matinee" || this.release === "nonFirstRelease") {
     return this.price - 2;
   } else {
     return this.price;  
